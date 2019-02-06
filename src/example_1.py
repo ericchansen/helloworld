2b1f29c48508b89e5e3d5373f868d4bad36db365
Eric Hansen <eric.a.hansen@avanade.com>
2019-02-06 15:09:17 -0600

    Hello world!
    This repo serves as a collection of resources for the aspiring data
    scientist. I hope to update it regularly, and I also hope you will
    contribute!

---
+++ src/example_1.py
@@ -0,0 +1,32 @@
+print(
+"""
+The Python interpreter starts at the top and works it's way down.
+"""
+)
+print(
+"""
+Python uses namespaces to manage variables/environments. For example,
+since we ran this from the command line, we're in the __main__ namespace.
+"""
+)
+
+def main():
+    print(
+"""
+I'm inside the main function! You don't need to have a main "
+function, but it's good practice!
+""")
+
+if __name__ == "__main__":
+    print(
+"""
+This is where the program *should* start (if we follow good object oriented
+Python practices). Python doesn't prevent you from doing dumb things.
+You're free to do it as you please!
+"""
+)
+    main()
+
+    # This is how you write comments in Python.
+    # import import_this
+    # print(import_this.a)
---
+++ src/example_1_import_this.py
@@ -0,0 +1,9 @@
+print('-' * 79)
+print("I'm in a different Python script!")
+print()
+print("This is my namespace: {}".format(__name__))
+print()
+print("Notice how it's no longer __main__?")
+print('-' * 79)
+
+a = "cats"
